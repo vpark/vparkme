7a91b73fd35a7f03cfc0fcf5e5c8d1b45e4a0193
Vincent Park <vincentpark@gmail.com>
2013-12-16 16:34:50 -0800

    modified application.js to ignore require_tree && finalized all link and pictures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,9 @@
 //= require underscore
 //= require backbone
 //= require vpark
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
-//= require_tree .
+// require_tree ../templates
+// require_tree ./models
+// require_tree ./collections
+// require_tree ./views
+// require_tree ./routers
+// require_tree .
