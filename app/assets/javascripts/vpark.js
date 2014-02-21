179ad302b3e31b1ef3e0d596599cf6a6827ff80c
Vincent Park <vincentpark@gmail.com>
2014-02-20 20:57:10 -0800

    added #scrollTop effects for the navbar

--- app/assets/javascripts/vpark.js
+++ app/assets/javascripts/vpark.js
@@ -1,13 +1,13 @@
-window.Vpark = {
-  Models: {},
-  Collections: {},
-  Views: {},
-  Routers: {},
-  initialize: function(){
-    
-  }
+function vparkScroll(){
+  $(".button").click(function(el){
+    el.preventDefault();
+    var target = $(this).attr("href");
+    var adjustment = $(".jumbotron").height() + 30
+    var delay = 500;
+    $("body").animate({scrollTop: ($(target).position().top - adjustment)}, delay);
+  });
 };
 
 $(document).ready(function(){
-  Vpark.initialize();
+  vparkScroll();
 });
